6fd43b520393fbec6ad19482a9789b0a972691a6
George Mocioiu <93079924+geogabi@users.noreply.github.com>
2023-10-13 18:07:54 +0300

    Delete Articol directory

--- Articol/Articol/register/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.login_usr, name='login_usr'),
-    path('logout/', views.log_out, name='log_out'),
-]
