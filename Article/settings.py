1cec0f991a9fd38163ee91dcf38df76f3949dcce
George Mocioiu <93079924+geogabi@users.noreply.github.com>
2023-10-24 15:41:22 +0300

    Add files via upload

--- Article/settings.py
+++ Article/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zucu7mqsf!t((hx#lznlfdkfhd947v$b7_(9vi-jzcx8n77th!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,6 +130,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
